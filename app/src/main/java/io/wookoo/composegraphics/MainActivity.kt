65b9ddb0d609b6d22c9178802f11dc1ce3ba81fd
genry <renegadeworking@gmail.com>
2024-12-09 21:56:30 +0300

    Draw Text

--- app/src/main/java/io/wookoo/composegraphics/MainActivity.kt
+++ app/src/main/java/io/wookoo/composegraphics/MainActivity.kt
@@ -1,8 +1,16 @@
 package io.wookoo.composegraphics
 
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.tooling.preview.Preview
 import io.wookoo.composegraphics.components.BallGame
 import io.wookoo.composegraphics.ui.theme.ComposeGraphicsTheme
 
