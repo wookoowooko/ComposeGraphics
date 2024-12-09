dc47cfda54eee7085d8e6dbb025e9a75b08ca5c4
genry <renegadeworking@gmail.com>
2024-12-09 18:57:05 +0300

    Ball Game

--- app/src/main/java/io/wookoo/composegraphics/MainActivity.kt
+++ app/src/main/java/io/wookoo/composegraphics/MainActivity.kt
@@ -3,19 +3,19 @@ package io.wookoo.composegraphics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-
+import io.wookoo.composegraphics.components.BallGame
 import io.wookoo.composegraphics.ui.theme.ComposeGraphicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             ComposeGraphicsTheme {
-
+                BallGame()
             }
         }
     }
 }
+
 
